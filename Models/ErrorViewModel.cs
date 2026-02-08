4c752d786ce3195699b7e903fb4597a76be3a91d
EmreSeyfula <emre.islam198@gmail.com>
2026-02-08 12:10:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeReadtForExam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
